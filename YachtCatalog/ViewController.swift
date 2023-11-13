a8dd5fa1cefd5902d928a6d53da63bed8925ca5c
Aleksei Izmalkin <aai95@yandex.ru>
2023-11-13 15:50:30 +0400

    Remove redundant files from project

--- YachtCatalog/ViewController.swift
+++ YachtCatalog/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  YachtCatalog
-//
-//  Created by Алексей on 13.11.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGreen
     }
-
-
 }
-
